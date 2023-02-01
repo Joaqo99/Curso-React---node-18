7fbf9a9b7cf42ca74856c776e3a999a6b58c4dda
Joaquín Varela <joaqo9909@gmail.com>
2023-02-01 18:19:17 -0300

    Redux toolkit

--- 09-toolkit-redux/src/App.jsx
+++ 09-toolkit-redux/src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { increment, decrement, incrementBy } from './store/slices/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector(state => state.counter)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>count is { counter }</p>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button type='button' onClick={()=>{dispatch(increment())}}>
+          Increment
+        </button>
+        <button type='button' onClick={()=>{dispatch(incrementBy(2))}}>
+          Increment by 2
+        </button>
+
+        <button type='button' onClick={()=>{dispatch(decrement())}}>
+          Decrement
         </button>
       </div>
     </div>
